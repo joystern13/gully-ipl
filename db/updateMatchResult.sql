ed7cbc6e50d71946fe6ca52001629d6e91c3c61d
joystern13 <38117842+joystern13@users.noreply.github.com>
2018-05-21 17:17:37 +0200

    Additional points for eliminators and final

--- db/updateMatchResult.sql
+++ db/updateMatchResult.sql
@@ -8,6 +8,7 @@ DECLARE losingCount int;
     DECLARE msg TEXT;
     DECLARE rows INT;
     DECLARE result TEXT;
+    DECLARE multiplier INT; --extra points for qualifiers and final
     
     DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
     BEGIN
@@ -23,7 +24,15 @@ DECLARE losingCount int;
         user_vote_master
     WHERE
         matchid = matchId_ AND teamid <> teamId_;
-      
+
+    IF matchId_ < 7950 THEN
+        multiplier = 1;
+    ELSE IF matchId_ = 7593
+        multiplier = 4;
+    ELSE
+        multiplier = 2;
+    END IF;
+
     SELECT 
         COUNT(1)
     INTO winningCount FROM
@@ -35,8 +44,8 @@ DECLARE losingCount int;
         set winningPoints = 0;
         set losingPoints = 0;
     else
-        set winningPoints = losingCount/winningCount;
-        set losingPoints = -1;
+        set winningPoints = multiplier*losingCount/winningCount;
+        set losingPoints = -1 * multiplier;
     end if;
     
     UPDATE match_master 
